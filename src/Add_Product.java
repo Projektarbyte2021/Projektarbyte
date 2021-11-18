f0ea04b9a04229e17ce3dc90c663f77b8528217c
ff03de <Fynn_Federer@web.de>
2021-11-18 19:42:09 +0100

    Deleted unused Files and add BufferedWriter

--- src/Add_Product.java
+++ src/Add_Product.java
@@ -1,7 +1,8 @@
 import javax.swing.*;
+import java.io.*;
 
-public class Add_Product extends JFrame {
-    JButton close, back;
+public class Add_Product extends JFrame  {
+    JButton close, back, add;
     JTextArea nproduct, cproduct;
     JLabel name;
     public Add_Product() {
@@ -19,24 +20,39 @@ public class Add_Product extends JFrame {
     back = new JButton("Back");
     back.setBounds(450, 500, 200, 50);
     
+    // add JButton for Add
+    add = new JButton("Add");
+    add.setBounds(450, 100, 200, 50);
+    
     // add JLabel for Name of Product
     name = new JLabel("Name of Product:");
     name.setBounds(100, 50, 100, 10);
     
     // add JTextArea for Name of product
-    nproduct = new JTextArea();
+    nproduct = new JTextArea("Name: ");
     nproduct.setBounds(210, 47, 100, 20);
     
     // add Actionlisteners
     close.addActionListener(a -> {System.exit(0);});
     back.addActionListener(b -> {this.dispose(); new Main();});
+    add.addActionListener(c -> {
+		try{File file = new File("");
+            JFileChooser fc = new JFileChooser();
+            fc.showSaveDialog(null);
+            file = fc.getSelectedFile();
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+            bw.write(nproduct.getText());
+            bw.flush();
+		}catch (IOException e) {System.out.println("Datei kann nicht erstellt werden");}
+	});
     
     // add JButtons to JFrame
     this.add(close);
     this.add(back);
+    this.add(add);
     this.add(name);
     this.add(nproduct);
-	}
+  }
 }
   
 
