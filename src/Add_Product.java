bc968a99e1c3a6f3e10ec39c34b1f7ab7228ae3b
ff03de <Fynn_Federer@web.de>
2021-12-09 11:51:56 +0100

    New Code Structur

--- src/Add_Product.java
+++ src/Add_Product.java
@@ -1,79 +1,62 @@
-import javax.swing.*;  
-import java.awt.*;  
+import java.awt.*;
+import javax.swing.*;
 import java.io.*;
 
-public class Add_Product extends JFrame  {  
-    JPanel panel;
-    JButton close, back, add;
-    JRadioButton don, doff;
-    JTextArea nproduct, cproduct, pproduct, iproduct;
-    JLabel name, category, price, info; 
-  public Add_Product() {             
+/**
+ *
+ * Beschreibung
+ *
+ * @version 1.0 vom 09.12.2021
+ * @author 
+ */
+
+public class Add_Product extends JFrame {
+  // Anfang Attribute
+  private JButton close = new JButton();
+  private JButton back = new JButton();
+  private JButton add = new JButton();
+  private JRadioButton don = new JRadioButton("Description on");
+  private JRadioButton doff = new JRadioButton("Description off");
+  private JTextArea nproduct = new JTextArea("");
+  private JScrollPane nproductScrollPane = new JScrollPane(nproduct);
+  private JTextArea cproduct = new JTextArea("");
+  private JScrollPane cproductScrollPane = new JScrollPane(cproduct);
+  private JLabel name = new JLabel();
+  private JLabel category = new JLabel();
+  private JTextArea pproduct = new JTextArea("");
+  private JScrollPane pproductScrollPane = new JScrollPane(pproduct);
+  private JLabel price = new JLabel();
+  private JTextArea iproduct = new JTextArea("");
+    private JScrollPane iproductScrollPane = new JScrollPane(iproduct);
+  private JLabel info = new JLabel();
+  // Ende Attribute
+  
+  public Add_Product() { 
+    // Frame-Initialisierung
     super("Verwaltungsprogramm - Add Product");
-    this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setVisible(true);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setExtendedState(JFrame.MAXIMIZED_BOTH);
+    setVisible(true);
+    Container cp = getContentPane();
+    cp.setLayout(null);
+    // Anfang Komponenten
     
-    // add JButton for Cancel
-    close = new JButton("Close");
     close.setBounds(200, 500, 200, 50);
-    
-    // add JButton for Back
-    back = new JButton("Back");
+    close.setText("Close");
+    close.addActionListener(a -> {
+      System.exit(0);
+    });
+    cp.add(close);
     back.setBounds(450, 500, 200, 50);
-    
-    // add JButton for Add
-    add = new JButton("Add");
+    back.setText("Back");
+    back.addActionListener(a -> {;
+      this.dispose();
+      new Main();
+    });
+    cp.add(back);
     add.setBounds(450, 100, 200, 50);
-    
-    // add JCheckBox for Description on
-    don = new JRadioButton("Description on");
-    
-    // add JCheckBox for Description off
-    doff = new JRadioButton("Description off");
-    
-    panel = new JPanel(); 
-    panel.setBackground(Color.gray);
-    
-    // add JLabel for Name of Product
-    name = new JLabel("Name of Product:");
-    name.setBounds(100, 50, 100, 10);
-    
-    // add JTextArea for Name of product
-    nproduct = new JTextArea("Name: ");
-    nproduct.setBounds(250, 47, 100, 20);
-    
-    // add JLabel for Category of Product
-    category = new JLabel("Category of Product:");
-    category.setBounds(100, 87, 200, 20);
-    
-    // add JTextArea for Category of product
-    cproduct = new JTextArea("Category: ");
-    cproduct.setBounds(250, 90, 100, 20);
-    
-    // add JLabel for Price of Product
-    price = new JLabel("Price of Product:");
-    price.setBounds(100, 150, 100, 10);
-    
-    // add JTextArea for Price of Product
-    pproduct = new JTextArea("Price: ");
-    pproduct.setBounds(250, 150, 100, 20);
-    
-    // add JLabel for Information of Product
-    info = new JLabel("Information of Product:");
-    info.setBounds(100, 200, 200, 10);
-    
-    // add JTextArea for Information of product
-    iproduct = new JTextArea("Information of Product: ");
-    iproduct.setBounds(250, 200, 200, 50);
-    
-    /*JComboBox cproduct = new JComboBox(windowList);
-        cproduct.setBounds(550,225, 200,50);*/
-    
-    // add Actionlisteners
-    close.addActionListener(a -> {System.exit(0);});
-    back.addActionListener(b -> {this.dispose(); new Main();});
-    add.addActionListener(c -> {
+    add.setText("Add");
+    add.addActionListener(a -> {
       try {
         File file = new File("");
         JFileChooser fc = new JFileChooser();
@@ -89,24 +72,48 @@ public class Add_Product extends JFrame  {
         bw.write(iproduct.getText());
         bw.flush();
       }catch (IOException e) {e.printStackTrace();}
-  });
+    });
+    don.addActionListener(a -> {;
+      nproduct.setText("Name: ");
+      cproduct.setText("Category: ");
+      pproduct.setText("Price: ");
+      iproduct.setText("Description: ");
+    });
+    doff.addActionListener(a -> {
+      nproduct.setText(" ");
+      cproduct.setText(" ");
+      pproduct.setText(" ");
+      iproduct.setText(" ");
+    });
     
-    // add JButtons to JFrame    
-    this.add(panel);
-    panel.add(close);
-    panel.add(back);
-    panel.add(add);
-    panel.add(don);
-    panel.add(doff);   
-    panel.add(name);
-    panel.add(info);
-    panel.add(price);
-    panel.add(category);
-    panel.add(nproduct);
-    panel.add(cproduct);
-    panel.add(iproduct);
-  }
+    cp.add(add);
+    don.setBounds(420, 38, 100, 20);
+    cp.add(don);
+    doff.setBounds(420, 73, 100, 20);
+    cp.add(doff);
+    nproductScrollPane.setBounds(130, 27, 168, 44);
+    cp.add(nproductScrollPane);
+    cproductScrollPane.setBounds(130, 80, 168, 44);
+    cp.add(cproductScrollPane);
+    name.setBounds(16, 34, 110, 20);
+    name.setText("Name of Product:");
+    cp.add(name);
+    category.setBounds(13, 90, 110, 20);
+    category.setText("Category of Product:");
+    cp.add(category);
+    pproductScrollPane.setBounds(130, 137, 168, 44);
+    cp.add(pproductScrollPane);
+    price.setBounds(12, 150, 110, 20);
+    price.setText("Price of Product:");
+    cp.add(price);
+    iproductScrollPane.setBounds(129, 194, 168, 44);
+    cp.add(iproductScrollPane);
+    info.setBounds(9, 211, 110, 20);
+    info.setText("Description of Product: ");
+    cp.add(info);
+    // Ende Komponenten
+  } 
+  
 }
   
-
-
+ 
