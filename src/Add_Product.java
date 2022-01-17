a14bbfa5f2cd06ec0987c7c47c7556f560d2b568
ff03de <Fynn_Federer@web.de>
2022-01-17 20:23:39 +0100

    Add new Code Structure

--- src/Add_Product.java
+++ src/Add_Product.java
@@ -7,46 +7,52 @@ import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Add_Product extends JFrame {
-                                 
+
   private FileFilter filter;
-  private JButton close, back, add, show;
+  private JButton close, back, add, show, alldelete, onedelete;
   private JRadioButton donoff;
-  private JTextArea nproduct, pproduct, iproduct, inventoryproduct, read;
+  private JTextArea nproduct;
+  private JTextArea pproduct;
+  private JTextArea iproduct;
+  private JTextArea inventoryproduct;
+  private static JTextArea read;
   private JComboBox c;
   private JScrollPane nproductScrollPane, pproductScrollPane, iproductScrollPane, inventoryScrollPane, readScrollPane;
   private JLabel name, category, price, info, inventory;
-  private String zv1, zv2, zv3, zv4, zv5, electronic, mechanic, social, services, error ="non";
+  private String zv1, zv2, zv3, zv4, zv5, electronic, mechanic, social, services, error = "non";
   private boolean test1, test2, test3, test4, test5, check = false;
-  
-  public Add_Product() { 
+  private Add_Product[] capacity;
+  private String mod_deleted;
+
+  public Add_Product() {
     super("Verwaltungsprogramm - Add Product");
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setExtendedState(JFrame.MAXIMIZED_BOTH);
     setVisible(true);
     Container cp = getContentPane();
     cp.setLayout(null);
-    
+
     // JTextAreas and JScrollPanes
     nproduct = new JTextArea();
     nproductScrollPane = new JScrollPane(nproduct);
     nproductScrollPane.setBounds(150, 27, 168, 44);
     cp.add(nproductScrollPane);
-     
+
     String[] categoryList = {"Please select a category", "electronic", "mechanic", "social", "services"};
     c = new JComboBox(categoryList);
     c.setBounds(150, 80, 168, 44);
     cp.add(c);
-    
+
     pproduct = new JTextArea();
     pproductScrollPane = new JScrollPane(pproduct);
     pproductScrollPane.setBounds(150, 137, 168, 44);
     cp.add(pproductScrollPane);
-    
+
     inventoryproduct = new JTextArea();
     inventoryScrollPane = new JScrollPane(inventoryproduct);
     inventoryScrollPane.setBounds(150, 194, 168, 44);
     cp.add(inventoryScrollPane);
-    
+
     iproduct = new JTextArea();
     iproductScrollPane = new JScrollPane(iproduct);
     iproductScrollPane.setBounds(150, 258, 168, 44);
@@ -56,37 +62,37 @@ public class Add_Product extends JFrame {
     readScrollPane = new JScrollPane(read);
     readScrollPane.setBounds(680, 27, 600, 600);
     cp.add(readScrollPane);
-    
+
     // JLabels
     name = new JLabel("Name of Product:");
     name.setBounds(16, 34, 150, 20);
     cp.add(name);
-    
+
     category = new JLabel("Category of Product:");
     category.setBounds(16, 90, 150, 20);
     cp.add(category);
-    
+
     price = new JLabel("Price of Product:");
     price.setBounds(16, 150, 150, 20);
     cp.add(price);
-    
+
     inventory = new JLabel("Inventory of Product: ");
     inventory.setBounds(16, 211, 150, 20);
     cp.add(inventory);
-    
+
     info = new JLabel("Description of Product: ");
     info.setBounds(16, 271, 150, 20);
     cp.add(info);
-    
+
     // JButtons
     close = new JButton("Close");
     close.setBounds(200, 500, 200, 50);
     cp.add(close);
-    
+
     back = new JButton("Back");
     back.setBounds(450, 500, 200, 50);
     cp.add(back);
-    
+
     add = new JButton("Add");
     add.setBounds(420, 100, 200, 50);
     cp.add(add);
@@ -97,14 +103,23 @@ public class Add_Product extends JFrame {
     show = new JButton(icon1);
     show.setBounds(420, 200, 200, 50);
     cp.add(show);
-    
+
+    alldelete = new JButton("Delete all Products");
+    alldelete.setBounds(420, 300, 200, 50);
+    this.add(alldelete);
+
+    onedelete = new JButton("Delete one Product");
+    onedelete.setBounds(420, 400, 200, 50);
+    this.add(onedelete);
+
     // JRadioButton
     donoff = new JRadioButton("Label off");
     donoff.setBounds(420, 50, 100, 20);
     cp.add(donoff);
-    
+
     // ActionListener for JRadioButton 
-    donoff.addActionListener(d -> {;
+    donoff.addActionListener(d -> {
+      ;
       if (check == false) {
         donoff.setText("Label on");
         nproduct.setText("Name: ");
@@ -117,28 +132,28 @@ public class Add_Product extends JFrame {
         inventoryproduct.setText("Inventory of Product: ");
         iproduct.setText("Description: ");
         check = true;
-      } else if (check == true) 
-        {
-          donoff.setText("Label off");
-          nproduct.setText(" ");
-          test1 = false;
-          test2 = false;
-          test3 = false;
-          test4 = false;
-          test5 = false;
-          pproduct.setText(" ");
-          inventoryproduct.setText(" ");
-          iproduct.setText(" ");
-          check = false;
-        } else {
-          ErrorDialog error = new ErrorDialog();
-          error.setLabelError();
-          }
-      
-    });                     
-    
+      } else if (check == true) {
+        donoff.setText("Label off");
+        nproduct.setText(" ");
+        test1 = false;
+        test2 = false;
+        test3 = false;
+        test4 = false;
+        test5 = false;
+        pproduct.setText(" ");
+        inventoryproduct.setText(" ");
+        iproduct.setText(" ");
+        check = false;
+      } else {
+        ErrorDialog error = new ErrorDialog();
+        error.setLabelError();
+      }
+
+    });
+
     // ItemListener
-    c.addItemListener(ia -> {;
+    c.addItemListener(ia -> {
+      ;
       if (c.getSelectedItem().equals("Please select a category")) {
         if (test1 == true) {
           error = "Category: non";
@@ -147,7 +162,7 @@ public class Add_Product extends JFrame {
           social = "";
           services = "";
         } // end of if
-        
+
         if (test1 == false) {
           error = "non";
           electronic = "";
@@ -155,8 +170,8 @@ public class Add_Product extends JFrame {
           social = "";
           services = "";
         } // end of if
-      } 
-      
+      }
+
       if (c.getSelectedItem().equals("electronic")) {
         if (test2 == true) {
           error = "";
@@ -165,7 +180,7 @@ public class Add_Product extends JFrame {
           social = "";
           services = "";
         } // end of if
-        
+
         if (test2 == false) {
           error = "";
           electronic = "electronic";
@@ -173,8 +188,8 @@ public class Add_Product extends JFrame {
           social = "";
           services = "";
         } // end of if
-      } 
-      
+      }
+
       if (c.getSelectedItem().equals("mechanic")) {
         if (test3 == true) {
           error = "";
@@ -183,7 +198,7 @@ public class Add_Product extends JFrame {
           social = "";
           services = "";
         } // end of if
-        
+
         if (test3 == false) {
           error = "";
           electronic = "";
@@ -191,8 +206,8 @@ public class Add_Product extends JFrame {
           social = "";
           services = "";
         } // end of if
-      } 
-      
+      }
+
       if (c.getSelectedItem().equals("social")) {
         if (test4 == true) {
           error = "";
@@ -201,7 +216,7 @@ public class Add_Product extends JFrame {
           social = "Category: social";
           services = "";
         } // end of if
-        
+
         if (test4 == false) {
           error = "";
           electronic = "";
@@ -209,8 +224,8 @@ public class Add_Product extends JFrame {
           social = "social";
           services = "";
         } // end of if
-      } 
-      
+      }
+
       if (c.getSelectedItem().equals("services")) {
         if (test5 == true) {
           error = "";
@@ -219,7 +234,7 @@ public class Add_Product extends JFrame {
           social = "";
           services = "Category: services";
         } // end of if
-        
+
         if (test5 == false) {
           error = "";
           electronic = "";
@@ -233,26 +248,26 @@ public class Add_Product extends JFrame {
     close.addActionListener(a -> {
       System.exit(0);
     });
-     
+
     back.addActionListener(b -> {
       this.dispose();
       new Gui();
     });
-    
+
     add.addActionListener(c -> {
       JFileChooser fc = new JFileChooser();
       filter = new FileNameExtensionFilter("Textdatei", "txt");
-      fc.addChoosableFileFilter(filter);   
+      fc.addChoosableFileFilter(filter);
       int i = fc.showSaveDialog(null);
-      if(i == JFileChooser.APPROVE_OPTION) {
+      if (i == JFileChooser.APPROVE_OPTION) {
         try {
           boolean neu = false;
           File file;
           file = fc.getSelectedFile();
-          if(!file.exists()) {
+          if (!file.exists()) {
             file.createNewFile();
             neu = true;
-          }      
+          }
           BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
           if (!neu) {
             bw.write("\n");
@@ -267,39 +282,39 @@ public class Add_Product extends JFrame {
             this.zv2 = error;
           } else if (error == "Category: non") {
             bw.write(error);
-            this.zv2 = error;  
+            this.zv2 = error;
           }
-          
+
           if (electronic == "electronic") {
             bw.write(electronic);
             this.zv2 = electronic;
           } else if (electronic == "Category: electronic") {
             bw.write(electronic);
-            this.zv2 = electronic;  
-          } 
-            
+            this.zv2 = electronic;
+          }
+
           if (mechanic == "mechanic") {
             bw.write(mechanic);
             this.zv2 = mechanic;
           } else if (mechanic == "Category: mechanic") {
             bw.write(mechanic);
-            this.zv2 = mechanic;  
+            this.zv2 = mechanic;
           }
-           
+
           if (social == "social") {
             bw.write(social);
             this.zv2 = mechanic;
           } else if (social == "Category: social") {
             bw.write(social);
-            this.zv2 = mechanic;  
+            this.zv2 = mechanic;
           }
-           
+
           if (services == "services") {
             bw.write(services);
             this.zv2 = services;
           } else if (services == "Category: services") {
             bw.write(services);
-            this.zv2 = services;  
+            this.zv2 = services;
           }
           bw.write("\n");
           bw.write(pproduct.getText());
@@ -311,24 +326,24 @@ public class Add_Product extends JFrame {
           bw.write(iproduct.getText());
           this.zv5 = iproduct.getText();
           bw.flush();
-          if(i == JFileChooser.APPROVE_OPTION) {
+          if (i == JFileChooser.APPROVE_OPTION) {
             File f = fc.getSelectedFile();
             String filepath = f.getPath();
             try {
               BufferedReader br = new BufferedReader(new FileReader(filepath));
-              String s1 = "",s2 = "";
-              while((s1 = br.readLine()) != null){
-                s2+=s1+"\n";
+              String s1 = "", s2 = "";
+              while ((s1 = br.readLine()) != null) {
+                s2 += s1 + "\n";
               }
               read.setText(s2);
               br.close();
-            } catch(Exception e) {
+            } catch (Exception e) {
               // e.printStackTrace();
               ErrorDialog error = new ErrorDialog();
               error.setOpenError();
             }
           }
-        }catch (IOException e) {
+        } catch (IOException e) {
           // e.printStackTrace();
           ErrorDialog error = new ErrorDialog();
           error.setWriteError();
@@ -344,46 +359,123 @@ public class Add_Product extends JFrame {
       fc.addChoosableFileFilter(filter);
       int i = fc.showOpenDialog(this);
 
-      if(i == JFileChooser.APPROVE_OPTION) {
+      if (i == JFileChooser.APPROVE_OPTION) {
         File f = fc.getSelectedFile();
         String filepath = f.getPath();
         try {
           BufferedReader br = new BufferedReader(new FileReader(filepath));
-          String s1 = "",s2 = "";
-          while((s1 = br.readLine()) != null){
-            s2+=s1+"\n";
+          String s1 = "", s2 = "";
+          while ((s1 = br.readLine()) != null) {
+            s2 += s1 + "\n";
           }
           read.setText(s2);
           br.close();
-        } catch(Exception e) {
+        } catch (Exception e) {
           // e.printStackTrace();
           ErrorDialog error = new ErrorDialog();
           error.setOpenError();
         }
       }
     });
-  } 
-  
+
+    alldelete.addActionListener(e -> {
+      try {
+        File file;
+        JFileChooser fc = new JFileChooser();
+        fc.showSaveDialog(null);
+        file = fc.getSelectedFile();
+        File f = fc.getSelectedFile();
+        String filepath = f.getPath();
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+        bw.write("");
+        bw.flush();
+        BufferedReader br = new BufferedReader(new FileReader(filepath));
+        String s1 = "", s2 = "";
+        while ((s1 = br.readLine()) != null) {
+          s2 += s1 + "\n";
+        }
+        read.setText(s2);
+        br.close();
+
+      } catch (Exception ex) {
+        // e.printStackTrace();
+        ErrorDialog error = new ErrorDialog();
+        error.setWriteError();
+      }
+    });
+
+    onedelete.addActionListener(d -> {
+      Add_Product[] zwischenvariabel = null;
+      int x = JOptionPane.showConfirmDialog(null, "Moechten Sie ein Produkt loeschen?");
+      if (x == 0) {
+        String input = JOptionPane.showInputDialog("Geben Sie die Kategory ein, die Sie loeschen moechten:");
+        try {
+          File file;
+          JFileChooser fc = new JFileChooser();
+          fc.showSaveDialog(null);
+          file = fc.getSelectedFile();
+          File f = fc.getSelectedFile();
+          String filepath = f.getPath();
+          boolean neu = true;
+          boolean deleted = false;
+          if (file.exists()) {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file, false));
+            for (Add_Product element : capacity) {
+              if (!(mod_deleted.equals(element.getCategory()))) {
+                if (!neu) {
+                  bw.write("\n");
+                } else {
+                  neu = false;
+                }
+              } else {
+                deleted = true;
+              }
+            }
+            bw.close();
+          } else {
+            ErrorDialog error = new ErrorDialog();
+            error.setWrongFile();
+          }
+          if (deleted) {
+            ErrorDialog error = new ErrorDialog();
+            error.setProductDelete();
+          } else {
+            ErrorDialog error = new ErrorDialog();
+            error.setWrongItemNumber();
+          }
+          BufferedReader br = new BufferedReader(new FileReader(filepath));
+          String s1 = "", s2 = "";
+          while ((s1 = br.readLine()) != null) {
+            s2 += s1 + "\n";
+          }
+          read.setText(s2);
+          br.close();
+        } catch (IOException ioAusnahme) {
+          ErrorDialog error = new ErrorDialog();
+          error.setOpenError();
+        }
+      }
+    });
+  }
+
   // Methods
   public String getName() {
-    return zv1;   
+    return zv1;
   }
-  
+
   public String getCategory() {
     return zv2;
   }
-  
+
   public String getPrice() {
     return zv3;
   }
-  
+
   public String getInventory() {
     return zv4;
   }
-  
+
   public String getInformation() {
     return zv5;
   }
-
-
 }
