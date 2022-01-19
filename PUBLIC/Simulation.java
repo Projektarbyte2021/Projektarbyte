ebad3384690d577e92a76a4c7b6a289ff42c21a7
KontraPRO <93860075+KontraPRO@users.noreply.github.com>
2022-01-19 15:32:40 +0100

    Add files via upload
    dropdownmenü in menü^deletproduct

---
+++ PUBLIC/Simulation.java
@@ -0,0 +1,13 @@
+// Imports
+import javax.swing.*;
+
+public class Simulation extends JFrame {
+
+  public Simulation() {
+    super("Verwaltungsprogramm - Simulation");
+    this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setLayout(null);
+    this.setVisible(true);
+  }
+}
