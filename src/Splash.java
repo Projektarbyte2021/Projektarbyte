8f9ad90d686b74ce6f6c74a7fcff5d7510210c1f
fynnfederer <Fynn_Federer@web.de>
2022-06-26 15:02:53 +0200

    Add Comments

--- src/Splash.java
+++ src/Splash.java
@@ -5,53 +5,44 @@ import javax.swing.*;
 public class Splash extends Variables{
 
   public Splash() {
-    infoLabel.setText("Easy to Manage 1.0.0");
-    URL iconurl = getClass().getResource("Pencil.png");
-    logo = new ImageIcon(iconurl);
-    container = window.getContentPane();
+    infoLabel.setText("Easy to Manage 1.0.0"); // Dem Attribut infoLabel vom Typ JLabel wird ein Text zugewiesen
+    URL iconurl = getClass().getResource("Pencil.png"); // Das Attribut iconurl vom Typ URL bekommt das Bild Pencil.png 
+    logo = new ImageIcon(iconurl); // Dem Attribut logo vom Typ ImageIcon wird das Bild aus iconurl zugewiesen
+    container = window.getContentPane(); 
     container.setLayout(new BorderLayout());
-    label = new JLabel(logo);
-
-    panel.setBorder(BorderFactory.createLineBorder(Color.black));
+    label = new JLabel(logo); // Das Bild wird zum JLabel label hinzugefügt
     
-    panel.add(label, BorderLayout.CENTER);
-    panel.add(infoLabel, BorderLayout.SOUTH);
+    panel.add(label, BorderLayout.CENTER); // Das JLabel label wird damit in der Mitte vom JPanel panel angezeigt
+    panel.add(infoLabel, BorderLayout.SOUTH); // Das JLabel infoLabel wir damit auf der unteren Seite vom JPanel panel angezeigt
     
-    container.add(panel);
+    container.add(panel); // Dem Container wird das JPanel panel zugewiesen
     
     
-    window.setBounds(30, 80, 486, 486);
-    window.setLocationRelativeTo(null);
-    window.setVisible(true);
+    window.setBounds(30, 80, 486, 486); // Das JWindow window bekommt damit die Position und die Größe des Fensters
+    window.setLocationRelativeTo(null); // Legt die Position des Fensters relativ zur angegebenen Komponente fest 
+    window.setVisible(true); // Mit diesem Befehl wird das JWindow angezeigt 
     
-    progress.setSize(486, 20);
+    progress.setSize(486, 20); // Die JProgressBar progress bekommt die Länge des JWindows und die Höhe 20
     
-    progress.setValue(0);
+    progress.setValue(0); // progress bekommt den Anfagnswert 0
     
-    progress.setStringPainted(true);
+    progress.setStringPainted(true); // Erlaubt progress den Wert als Text anzuzeigen und zu ändern
     
-    panel.add(progress);
+    panel.add(progress); // progress wird panel hinzugefügt
     
-    container.add(panel);
-    
-    for (int i = 0; i <= progress.getMaximum(); i++) { // The for loop runs until the end of the progressbar
-      progress.setValue(i);
+    for (int i = 0; i <= progress.getMaximum(); i++) { // Die Schleife läuft solange bis der MaximaleWert von progress erreicht wurde
+      progress.setValue(i); // progress bekommt immer den Wert von i
       try {
-        Thread.sleep(15);
+        Thread.sleep(15); // Dient zur Geschwindigkeit von prgress
       } catch (InterruptedException e) {
-        e.printStackTrace();
+        e.printStackTrace(); // Falls ein Fehler auftritt wird dieser in der Konsole ausgegeben
       }
       
     }
   }
 
   public void showSplash() {
-    window.setVisible(true);
-    window.dispose();
-  }
-
-  public void updateInfoLabel(String info) {
-    infoLabel.setText(info);
+    window.dispose(); // Schließt das JWindow wieder aber beendet nicht das Programm
   }
 
 }
